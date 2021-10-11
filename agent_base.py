4cd07a56d3f05e5926f6fb3f5b13a36039a257a0
yugeshizu <yuge.tatsuro@shizuoka.ac.jp>
2021-10-11 17:47:44 +0900

    agentクラスのベース

---
+++ agent_base.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sontoku import Sontoku
+
+
+STATES = Sontoku.STATES
+ACTIONS = Sontoku.ACTIONS
+
+
+class Agent:
+    def __init__(self):
+        # policy_default: Always choose action=0
+        policy_default = np.zeros((len(ACTIONS), len(STATES)))
+        policy_default[0, :] = 1
+        self.policy = policy_default
+
+    def get_initial_policy(self, state=STATES[0]):
+        return self.policy
+
+    def get_updated_policy(self, reward=0, next_state=STATES[0]):
+        return self.policy
+
+
+if __name__ == "__main__":
+    from play import play_a_game_auto
+
+    agent = Agent()
+    rewards_rec = play_a_game_auto(10, agent)
+    for i, reward in enumerate(rewards_rec):
+        print(
+            "Episode={},  reward={},  total_reward={}".format(
+                i, reward, sum(rewards_rec[: i + 1])
+            )
+        )
