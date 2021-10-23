bb7cabbfe69965730f28f257f70421621cc65790
urotuta <urotutageyu@gmail.com>
2021-10-23 16:04:39 +0900

    自動プレイの流れを変更

--- autoplay.py
+++ autoplay.py
@@ -12,26 +12,20 @@ def play_a_game_auto(total_episodes=10, agent=None):
     state = game.state
     rewards_rec = [0]
     if agent is not None:
-        policy = agent.get_initial_policy(state)
-        reward, state = episode(game, rewards_rec, policy[:, state])
-        for _ in range(total_episodes - 1):
-            policy = agent.get_updated_policy(reward, state)
-            reward, state = episode(game, rewards_rec, policy[:, state])
+        agent.initialize()
+        for _ in range(total_episodes):
+            action = agent.get_next_action()
+            reward, state = game.play_an_episode(action)
+            rewards_rec.append(reward)
+            agent.updated_policy(reward, state)
     else:
         # policy_default: Always choose action=0
-        policy_default = np.zeros((len(ACTIONS), len(STATES)))
-        policy_default[0, :] = 1
+        action = 0
         for _ in range(total_episodes):
-            reward, state = episode(game, rewards_rec, policy_default[:, state])
+            reward, state = game.play_an_episode(action)
+            rewards_rec.append(reward)
 
     return rewards_rec
-
-
-def episode(game, rewards_rec, policy):
-    action = np.random.choice(ACTIONS, p=policy)
-    reward, state = game.play_an_episode(action)
-    rewards_rec.append(reward)
-    return reward, state
 
 
 if __name__ == "__main__":
